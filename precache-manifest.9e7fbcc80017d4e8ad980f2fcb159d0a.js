559b09b8b8577f3e7a9e772e75b0658bbdfc146a
Matias <livingstonmatias@gmail.com>
2020-05-31 17:23:41 -0300

    Updates

---
+++ precache-manifest.9e7fbcc80017d4e8ad980f2fcb159d0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f8d57f546206ecf04d7a5cac26ed0e",
+    "url": "/Peliculas/index.html"
+  },
+  {
+    "revision": "35e10cb0631ac5f846ac",
+    "url": "/Peliculas/static/css/main.fe95742b.chunk.css"
+  },
+  {
+    "revision": "ab4edc49791d039d987c",
+    "url": "/Peliculas/static/js/2.62b3c70d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Peliculas/static/js/2.62b3c70d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35e10cb0631ac5f846ac",
+    "url": "/Peliculas/static/js/main.17683e5a.chunk.js"
+  },
+  {
+    "revision": "0e1d30f66eeb2607ec91",
+    "url": "/Peliculas/static/js/runtime-main.badc92e4.js"
+  }
+]);
